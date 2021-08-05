a70a58e258a5d7931dabceda6cd5c42b04b64a4c
nightrain-vampire <1402357917@qq.com>
2021-08-05 12:46:15 +0800

    增加了时区转换

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from logging.handlers import RotatingFileHandler
 from logging.handlers import SMTPHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import os
 import logging
 
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
