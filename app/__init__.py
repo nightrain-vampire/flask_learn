056a4783c11701322c8d49e197bb801cd3a566f8
nightrain-vampire <1402357917@qq.com>
2021-08-05 13:14:52 +0800

    国际化功能

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from logging.handlers import SMTPHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
+from flask_babel import Babel, lazy_gettext as _l
 import os
 import logging
 
@@ -17,9 +20,15 @@ db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
